e0f57e4344d7d7c3705a4d8ac25af477d67b26dd
Alex Woodman <alexthegoodman@gmail.com>
2025-09-30 22:40:34 -0400

    init layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,64 @@
 "use client";
 
+import { MagnifyingGlassIcon } from "@phosphor-icons/react";
 import styled from "styled-components";
 
-// Create a Title component that'll render an <h1> tag with some styles
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: #bf4f74;
+const Logo = styled.h1`
+  font-size: 24px;
+  font-family: "IBM Plex Mono", monospace;
+  display: block;
+  margin-right: 16px;
 `;
 
-// Create a Wrapper component that'll render a <section> tag with some styles
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
+const Headline = styled.h1`
+  font-size: 32px;
+`;
+
+const Header = styled.header`
+  padding: 20px;
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid #e5e5e5;
+`;
+
+const SearchBar = styled.input`
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  box-sizing: border-box;
+`;
+
+const SearchButton = styled.button`
+  padding: 8px 12px;
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+const Body = styled.section`
+  padding: 20px;
+  font-family: "IBM Plex Mono", monospace;
 `;
 
 export default function Home() {
   return (
     <main>
-      <Wrapper>
-        <Title>Hello World!</Title>
-      </Wrapper>
+      <Header>
+        <Logo>MegaSearch</Logo>
+        <SearchButton>
+          <MagnifyingGlassIcon size={18} />
+        </SearchButton>
+        <SearchBar type="text" placeholder="Search..." />
+      </Header>
+      <Body>
+        <Headline>Welcome to MegaSearch</Headline>
+        <p>
+          Find all kinds of businesses. No email harvesting. Extremely
+          efficient.
+        </p>
+      </Body>
     </main>
   );
 }
